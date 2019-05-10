4cc8e6389a536f62959663f9bec63c1ba2a173fc
Sanghee Lee <sanghee911@hotmail.com>
2019-05-10 16:32:03 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const os = require('os');
 const PORT = 8000;
 const HOST = '0.0.0.0';
 const hostname = os.hostname();
-const color = GetEnvironmentVar('COLOR', 'blue');
+const color = GetEnvironmentVar('COLOR', 'red');
 
 // App
 const app = express();
